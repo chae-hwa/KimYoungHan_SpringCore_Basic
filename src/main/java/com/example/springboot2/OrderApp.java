7f92637aaf7ff8be943684017924c7ae5b143103
chae-hwa <ihj0519@naver.com>
2022-09-07 19:39:12 +0900

    이해1 예제만들기 - 주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/com/example/springboot2/OrderApp.java
@@ -0,0 +1,22 @@
+package com.example.springboot2;
+
+import com.example.springboot2.member.*;
+import com.example.springboot2.order.Order;
+import com.example.springboot2.order.OrderService;
+import com.example.springboot2.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order.toString());
+
+    }
+}
---
+++ src/test/java/com/example/springboot2/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.springboot2.order;
+
+import com.example.springboot2.member.Grade;
+import com.example.springboot2.member.Member;
+import com.example.springboot2.member.MemberService;
+import com.example.springboot2.member.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
