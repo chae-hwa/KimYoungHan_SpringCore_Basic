0ec5bb93e5a4377b6693f9e764bb03555777c927
chae-hwa <ihj0519@naver.com>
2022-09-07 20:01:30 +0900

    이해2 객체지향원리적용 - 새로운 할인 정책 개발

---
+++ src/main/java/com/example/springboot2/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package com.example.springboot2.discount;
+
+import com.example.springboot2.member.Grade;
+import com.example.springboot2.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
