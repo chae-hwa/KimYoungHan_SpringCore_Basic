0dd9b35a791fde11629015785ed122939f8d1b8f
chae-hwa <ihj0519@naver.com>
2022-09-21 00:58:20 +0900

    컴포넌트 스캔 - 필터

---
+++ src/test/java/com/example/springboot2/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.example.springboot2.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/com/example/springboot2/scan/filter/BeanB.java
@@ -0,0 +1,6 @@
+package com.example.springboot2.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+
+}
