3f2f8f6838b6daa2ecfb42dd532da87848c9e6f8
chae-hwa <ihj0519@naver.com>
2022-09-24 22:51:16 +0900

    컴포넌트 스캔 - 중복등록과 충돌(수동 vs 수동)

--- src/main/java/com/example/springboot2/AutoAppConfig.java
+++ src/main/java/com/example/springboot2/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.example.springboot2;
 
+import com.example.springboot2.member.MemberRepository;
+import com.example.springboot2.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,9 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes =Configuration.class)
 )
 public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
