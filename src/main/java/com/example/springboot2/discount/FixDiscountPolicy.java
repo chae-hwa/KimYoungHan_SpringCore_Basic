562af80e24cd2a377422dc33c07b0abcb966f8de
chae-hwa <ihj0519@naver.com>
2022-10-04 00:32:48 +0900

    의존관계 자동 주입 - 조회 빈이 2개 이상(문재)

--- src/main/java/com/example/springboot2/discount/FixDiscountPolicy.java
+++ src/main/java/com/example/springboot2/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.example.springboot2.discount;
 
 import com.example.springboot2.member.Grade;
 import com.example.springboot2.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component // 스프링빈에 등록되어서 RateDiscountPolicy와 충돌
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
--- src/main/java/com/example/springboot2/order/OrderServiceImpl.java
+++ src/main/java/com/example/springboot2/order/OrderServiceImpl.java
@@ -8,18 +8,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements  OrderService {
 
     // final을 붙이면 생성자주입에서 값이 사용되어야 한다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    // 생성자 주입 (DI 주입) @RequiredArgsConstructor가 자동으로 만들어줌.
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository; // final을 붙일 때 필수 사용
-//        this.discountPolicy = discountPolicy; // final을 붙일 때 필수 사용
-//    }
+    // 생성자 주입 (DI 주입) @RequiredArgsConstructor가 자동으로 만들어줌.
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository; // final을 붙일 때 필수 사용
+        this.discountPolicy = discountPolicy; // final을 붙일 때 필수 사용
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
