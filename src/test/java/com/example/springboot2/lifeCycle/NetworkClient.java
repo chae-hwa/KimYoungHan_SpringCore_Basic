a9c7b04a8622e61424c2e1ddd5f348bcbaed85a8
chae-hwa <ihj0519@naver.com>
2022-10-05 00:19:45 +0900

    빈 생명주기 콜백 -콜백지원2, 빈등록 초기화/소멸 메서드 사용

--- src/test/java/com/example/springboot2/lifeCycle/NetworkClient.java
+++ src/test/java/com/example/springboot2/lifeCycle/NetworkClient.java
@@ -6,8 +6,7 @@ public class NetworkClient {
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
+
     }
 
     public void setUrl(String url){
@@ -26,5 +25,16 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     public void disconnect(){
         System.out.println("close : " + url);
+    }
+
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
