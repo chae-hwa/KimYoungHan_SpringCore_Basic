6bf2700b0259bd4defa801528ed51fd821e412a8
chae-hwa <ihj0519@naver.com>
2022-10-05 00:53:32 +0900

    빈 생명주기 콜백 -콜백지원3, @PostConstruct, @PreDestroy 어노테이션 사용

--- src/test/java/com/example/springboot2/lifeCycle/NetworkClient.java
+++ src/test/java/com/example/springboot2/lifeCycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package com.example.springboot2.lifeCycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -27,12 +30,14 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
